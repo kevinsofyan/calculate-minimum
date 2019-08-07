708a53e91cf7f972bdd97ff90766ade97828b7d9
Kevin Sofyan <kevin@apps-foundry.com>
2019-08-08 02:00:25 +0700

    fix calculatefraction logic

--- src/components/CalculateMinimum/calculateFraction.js
+++ src/components/CalculateMinimum/calculateFraction.js
@@ -1,33 +1,32 @@
 export function calculateFraction(inputNumber) {
     let number = inputNumber;
-    const divider = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 100, 50];
-    let fraction = [];
+    const fraction = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 100 , 50];
+    let result = [];
 
-    for (let i= 0; i < divider.length; i++) {
-        const countNumber = Math.floor(number / divider[i]);
+    for (let i= 0; i<fraction.length; i++) {
+        let j = number;
+        let countNumber = 0;
 
-        if(divider[i] <= number) {
-            if (i === divider.length - 1 && number !==0) {
-                number = (divider[i] * countNumber) - number;
-                fraction.push(createFraction(countNumber,divider[i]));
-
-            } else {
-                fraction.push(createFraction(1,divider[i]));
-                number-=divider[i];
+        if (fraction[i] <= number) {
+            while (fraction[i] <= j) {
+                countNumber++;
+                number-=fraction[i];
+                j-=fraction[i];
             }
+            result.push(createFraction(countNumber, fraction[i]));
         }
     }
 
     if (number !== 0){
-        fraction.push(createFraction(0, number));
+        result.push(createFraction(0,number));
     }
 
-    return fraction;
+    return result;
 }
 
 function createFraction(count, number) {
     let fraction = {};
-    fraction.number = Math.abs(number);
+    fraction.number = number;
     fraction.count = count;
     return fraction;
 };
